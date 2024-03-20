baed1b4b85967f587158bb5b02084f21052cb962
JorklinR <jorklin2009@hotmail.com>
2024-03-19 22:15:22 -0600

    Create

---
+++ NoSQL_Proyecto/NoSQL_Proyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoSQL_Proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
